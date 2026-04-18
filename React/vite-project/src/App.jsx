eada4cdec97bf4c0cf2d448a383b1714f581b2c9
akhil joy <akhiljoy312@gmail.com>
2026-04-18 10:14:12 +0530

    c56

--- React/vite-project/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  const flag = false;
-  return (
-    <div className={flag ? "t1" : "t2"}>
-      as;dljjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjk
-    </div>
-  );
-}
-export default App;
