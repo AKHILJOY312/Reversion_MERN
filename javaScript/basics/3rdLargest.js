a8fb7df4f01bcc716977c6d7351a62ef27eb1149
akhil joy <akhiljoy312@gmail.com>
2026-04-21 13:47:42 +0530

    c59

--- javaScript/basics/3rdLargest.js
+++ javaScript/basics/3rdLargest.js
@@ -1,24 +1,22 @@
 function thirdLargest(arr) {
-  if (arr.length < 3) return null;
-
   let first = -Infinity,
     second = -Infinity,
     third = -Infinity;
 
   for (let num of arr) {
+    if (num === first || num === second || num === third) continue;
+
     if (num > first) {
       third = second;
       second = first;
       first = num;
-    } else if (num > second && num !== first) {
+    } else if (num > second) {
       third = second;
       second = num;
-    } else if (num > third && num !== second && num !== first) {
+    } else if (num > third) {
       third = num;
     }
   }
 
-  return third;
+  return third === -Infinity ? null : third;
 }
-
-console.log(thirdLargest([10, 20, 4, 45, 99])); // 20
