c1aa0ab800ae49287f1791499e31dfe92d110656
akhil joy <akhiljoy312@gmail.com>
2026-04-21 12:47:04 +0530

    c58

--- DSA_15/graph/cyclePrint.js
+++ DSA_15/graph/cyclePrint.js
@@ -1,57 +1,53 @@
 class Graph {
   constructor() {
-    this.graph = new Map();
+    this.graph = {};
   }
 
   addVertex(v) {
-    if (!this.graph.has(v)) {
-      this.graph.set(v, []);
-    }
+    if (!this.graph[v]) this.graph[v] = [];
   }
 
   addEdge(v1, v2) {
     this.addVertex(v1);
     this.addVertex(v2);
-
-    this.graph.get(v1).push(v2);
-    this.graph.get(v2).push(v1);
+    this.graph[v1].push(v2);
+    this.graph[v2].push(v1);
   }
 
   printCycles(start) {
     const visited = new Set();
-    const stack = new Set();
-    const cycles = [];
 
     const dfs = (node, parent, path) => {
       visited.add(node);
-      stack.add(node);
       path.push(node);
 
-      for (let neighbor of this.graph.get(node)) {
+      for (let neighbor of this.graph[node]) {
         if (!visited.has(neighbor)) {
           dfs(neighbor, node, path);
-        } else if (neighbor !== parent && stack.has(neighbor)) {
-          const cycleStart = path.indexOf(neighbor);
-          const cycle = path.slice(cycleStart);
-          cycle.push(neighbor);
-          cycles.push(cycle);
+        } else if (neighbor !== parent) {
+          // cycle found
+          const idx = path.indexOf(neighbor);
+          if (idx !== -1) {
+            console.log(path.slice(idx));
+          }
         }
       }
 
-      stack.delete(node);
       path.pop();
     };
 
     dfs(start, null, []);
-
-    return cycles;
   }
 }
-
 const g = new Graph();
 
 g.addEdge("A", "B");
 g.addEdge("B", "C");
-g.addEdge("C", "A");
+g.addEdge("C", "A"); // cycle 1
 
-console.log(g.printCycles("A"));
+g.addEdge("A", "D");
+g.addEdge("C", "D"); // cycle 2
+
+g.addEdge("D", "E"); // no cycle here
+
+g.printCycles("A");
